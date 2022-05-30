f7919864c3b9e252ea3b8d17a2d9505c3bc878ad
VadimSabadyr <105793141+VadimSabadyr@users.noreply.github.com>
2022-05-30 22:48:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,34 +2,23 @@ sticks = int(input("Number: "))
 user1 = input("name user 1: ")
 user2 = input("name user 2: ")
 
-while sticks > -5:
+while sticks > 0:
     step1 = int(input(f"Your move {user1}! : "))
-    if step1 >= 4:
-        print("You can't take more than 3 sticks!")
-        pass
-    if step1 <= 3:
+    if step1 >= 4 or sticks <= 0:
+        print("You can take 1, 2, 3 sticks!")
+    elif step1 <= 3:
         sticks = sticks - step1
     print(f"Stikcs left {sticks}!")
-    if sticks < 0:
-        sticks = sticks + step1
-        print("Wrong step!!!")
-        print(f"Stikcs left {sticks}!")
     if sticks == 0:
         print(f"You win {user1}, Congratulations!")
         break
 
-
     step2 = int(input(f"Your move {user2}! : "))
-    if step2 >= 4:
-        print("You can't take more than 3 sticks!")
-        pass
-    if step2 <= 3:
+    if step2 >= 4 or sticks <= 0:
+        print("You can take 1, 2, 3 sticks!")
+    elif step2 <= 3:
         sticks = sticks - step2
     print(f"Stikcs left {sticks}!")
-    if sticks < 0:
-        sticks = sticks + step2
-        print("Wrong step!!!")
-        print(f"Stikcs left {sticks}!")
     if sticks == 0:
         print(f"You win {user2}, Congratulations!")
         break
